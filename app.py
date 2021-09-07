59b82bc3916adf09efa58cb43768032b6dcf97a5
mike-vestergaard <70510215+mike-vestergaard@users.noreply.github.com>
2021-09-07 16:36:06 +0200

    app testing
    testing

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ from datetime import timedelta
 
 app = Flask(__name__)
 # basically used for encrypt and de-crypt data
-app.secret_key = "mysecretkey"
-app.permanent_session_lifetime = timedelta(minutes=5)
+#app.secret_key = "mysecretkey"
+#app.permanent_session_lifetime = timedelta(minutes=5)
+
+# Test set-up of flask
+@app.route("/")
+def main():
+    return "<h1>Welcome!</h1>"
+
+if __name__ == "__main__":
+    app.run()
