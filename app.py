81086332c7c4a3e728ed74280f9b5bea4c518838
me2001ru <mehsum.rupani@yh.nackademin.se>
2021-09-07 22:30:30 +0200

    modified gitignore

--- app.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
-
-app = Flask(__name__)
-# basically used for encrypt and de-crypt data
-app.secret_key = "mysecretkey"
-app.permanent_session_lifetime = timedelta(minutes=5)
